8e62e94347964e2a7bba83537888dec53e51eb21
fl1pHover <bishu.hover@gmail.com>
2022-09-29 07:17:43 +0800

    asd

--- components/navbar.js
+++ components/navbar.js
@@ -13,6 +13,7 @@ import {
      UnorderedList,
      ListItem,
      Input,
+     GridItem,
 } from "@chakra-ui/react";
 import { Search2Icon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import NextLink from "next/link";
@@ -23,6 +24,8 @@ import { FaFacebookF } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { AddIcon } from "@chakra-ui/icons";
+
+import SectionLayout from "./layout/sectionLayout";
 
 const footerIcon = [
      {
@@ -70,170 +73,235 @@ const Navbar = (props) => {
      const { path } = props;
 
      const [sidebar, setSidebar] = useState(false);
+     const [mobilebar, setMobilebar] = useState(false);
 
      const showSidebar = () => setSidebar(!sidebar);
+     const showMobilebar = () => setMobilebar(!mobilebar);
 
      return (
-          <Box pos="relative" className="rel" zIndex="100">
-               <Box
-                    id="navbar"
-                    as="nav"
-                    height="100px"
-                    px="50px"
-                    display={{ base: "none", xl: "block" }}
-               >
-                    <Stack
-                         direction="row"
-                         justifyContent="space-between"
-                         alignItems="center"
-                         height="100%"
-                    >
-                         <Image
-                              boxSize="53px"
-                              objectFit="contain"
-                              src="../images/logo-1.png"
-                              alt="logo"
-                         />
-                         <Stack direction="row" gap={10}>
-                              <LinkItem href="/" path={path} text="Home">
-                                   <ChevronDownIcon />
-                              </LinkItem>
-                              <LinkItem
-                                   href="/portfolio"
-                                   path={path}
-                                   text="Portfolio"
-                              >
-                                   <ChevronDownIcon />
-                              </LinkItem>
+          <>
+               <Box display={{ base: "block", xl: "none" }} py="25px">
+                    <SectionLayout>
+                         <Stack direction="row" justifyContent="space-between">
+                              <Box w="25%" textAlign="left">
+                                   <HamburgerIcon
+                                        fontSize="25px"
+                                        fontWeight="bold"
+                                        color="white"
+                                        onClick={showMobilebar}
+                                   />
+                              </Box>
+                              <Box w="50%" textAlign="center">
+                                   Bili
+                              </Box>
+                              <Box w="25%" textAlign="right">
+                                   {""}
+                              </Box>
                          </Stack>
-                         <Stack direction="row" alignItems="center" spacing={5}>
-                              <IconButton
-                                   aria-label="Menu"
-                                   icon={<Search2Icon />}
-                                   borderRadius="50%"
-                                   fontSize="18px"
-                                   bg="tranparent"
-                                   _hover={{
-                                        bgColor: "transparent",
-                                   }}
-                              />{" "}
-                              <Center
-                                   className="menu__btn"
-                                   onClick={showSidebar}
-                              >
-                                   <Stack
-                                        position="relative"
-                                        className="menu__btn-wrap"
-                                        overflow="hidden"
-                                   >
-                                        {/* <span></span> */}
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                   </Stack>
-                              </Center>
-                         </Stack>
-                    </Stack>
-               </Box>
-
-               <Box
-                    pos="fixed"
-                    width="480px"
-                    right="0"
-                    p="50px"
-                    bg="mainBlack"
-                    height="100vh"
-                    className={sidebar ? "sidebar" : "sidebar active"}
-                    zIndex="101"
-               >
-                    <IconButton
-                         aria-label="Menu"
-                         icon={<CloseIcon />}
-                         borderRadius="50%"
-                         w="60px"
-                         h="60px"
-                         bg="mainGrey"
-                         fontSize="15px"
-                         pos="absolute"
-                         transform="translate(-140px, -25px)"
-                         transition="0.1s ease"
-                         _hover={{
-                              fontSize: "18px",
-                         }}
-                         onClick={showSidebar}
-                    />
-
-                    <Image
-                         src="./images/logo-1.png"
-                         alt="logo"
-                         width="50px"
-                         mt="50px"
-                         mb="25px"
-                    />
-                    <Text
-                         textStyle="quote"
-                         lineHeight="1.4"
-                         fontWeight="bold"
-                         my={10}
+                    </SectionLayout>
+                    <Box
+                         pos="fixed"
+                         width={{ base: "85%", sm: "400px" }}
+                         left="0"
+                         top="0"
+                         p="30px"
+                         bg="bgGrey"
+                         height="100vh"
+                         mr="30px"
+                         className={
+                              mobilebar ? "mobilebar" : "mobilebar activeMobile"
+                         }
+                         zIndex="101"
                     >
-                         Unique Digital Ideas for Successful Business
-                    </Text>
-                    <Box my={7}>
-                         <Text textStyle="title" mb={3}>
-                              Contact us
-                         </Text>
-                         <UnorderedList lineHeight="2">
-                              <ListItem>
-                                   27 Division St, New York, NY 10002
-                              </ListItem>
-                              <ListItem>+1 800 123 456 78</ListItem>
-                              <ListItem>bili@mail.com</ListItem>
-                         </UnorderedList>
+                         <Stack
+                              className="menu__btn-mobile"
+                              onClick={showMobilebar}
+                         >
+                              <span></span>
+                              <span></span>
+                              <span></span>
+                         </Stack>
+                         <Center>
+                              <Text textStyle="mainMediumHeader">Bili</Text>
+                         </Center>
+                         <Box h="30px" />
+                         <Stack direction="column">
+                              <Text>asdasd</Text>
+                              <Text>asdasd</Text>
+                              <Text>asdasd</Text>
+                              <Text>asdasd</Text>
+                         </Stack>
                     </Box>
-                    <Box my={10}>
-                         <Text textStyle="title" mb={3}>
-                              Subscribe
-                         </Text>
-                         <Input
-                              variant="flushed"
-                              focusBorderColor="none"
-                              w="100%"
-                              backgroundColor="transparent"
-                              borderBottomColor="mainGrey"
-                              placeholder="Get news & Updates"
-                              color="white"
-                              fontSize="18px"
-                              fontWeight="bold"
-                              mb="30px"
-                         />
-                         <Text fontSize="14px" color="titleGrey">
-                              Our expertise, as well as our passion for web
-                              design, sets us apart from other agencies.
-                         </Text>
+               </Box>
+               <Box pos="relative" className="rel" zIndex="100">
+                    <Box
+                         id="navbar"
+                         as="nav"
+                         height="100px"
+                         px="50px"
+                         display={{ base: "none", xl: "block" }}
+                    >
                          <Stack
                               direction="row"
-                              mt="50px"
-                              className="profile__social"
-                              gap={3}
+                              justifyContent="space-between"
+                              alignItems="center"
+                              height="100%"
                          >
-                              {footerIcon.map(({ ...props }, index) => {
-                                   return (
-                                        <NextLink href="/" passHref key={index}>
-                                             <Link
-                                                  fontSize="20px"
-                                                  _hover={{
-                                                       color: "mainOrange",
-                                                  }}
-                                             >
-                                                  {props.icons}
-                                             </Link>
-                                        </NextLink>
-                                   );
-                              })}
+                              <Image
+                                   boxSize="53px"
+                                   objectFit="contain"
+                                   src="../images/logo-1.png"
+                                   alt="logo"
+                              />
+                              <Stack direction="row" gap={10}>
+                                   <LinkItem href="/" path={path} text="Home">
+                                        <ChevronDownIcon />
+                                   </LinkItem>
+                                   <LinkItem
+                                        href="/portfolio"
+                                        path={path}
+                                        text="Portfolio"
+                                   >
+                                        <ChevronDownIcon />
+                                   </LinkItem>
+                              </Stack>
+                              <Stack
+                                   direction="row"
+                                   alignItems="center"
+                                   spacing={5}
+                              >
+                                   <IconButton
+                                        aria-label="Menu"
+                                        icon={<Search2Icon />}
+                                        borderRadius="50%"
+                                        fontSize="18px"
+                                        bg="tranparent"
+                                        _hover={{
+                                             bgColor: "transparent",
+                                        }}
+                                   />
+                                   <Center
+                                        className="menu__btn"
+                                        onClick={showSidebar}
+                                   >
+                                        <Stack
+                                             position="relative"
+                                             className="menu__btn-wrap"
+                                             overflow="hidden"
+                                        >
+                                             {/* <span></span> */}
+                                             <span></span>
+                                             <span></span>
+                                             <span></span>
+                                        </Stack>
+                                   </Center>
+                              </Stack>
                          </Stack>
                     </Box>
+
+                    <Box
+                         pos="fixed"
+                         width="480px"
+                         right="0"
+                         p="50px"
+                         bg="mainBlack"
+                         height="100vh"
+                         className={sidebar ? "sidebar" : "sidebar active"}
+                         zIndex="101"
+                    >
+                         <IconButton
+                              aria-label="Menu"
+                              icon={<CloseIcon />}
+                              borderRadius="50%"
+                              w="60px"
+                              h="60px"
+                              bg="mainGrey"
+                              fontSize="15px"
+                              pos="absolute"
+                              transform="translate(-140px, -25px)"
+                              transition="0.1s ease"
+                              _hover={{
+                                   fontSize: "18px",
+                              }}
+                              onClick={showSidebar}
+                         />
+
+                         <Image
+                              src="./images/logo-1.png"
+                              alt="logo"
+                              width="50px"
+                              mt="50px"
+                              mb="25px"
+                         />
+                         <Text
+                              textStyle="quote"
+                              lineHeight="1.4"
+                              fontWeight="bold"
+                              my={10}
+                         >
+                              Unique Digital Ideas for Successful Business
+                         </Text>
+                         <Box my={7}>
+                              <Text textStyle="title" mb={3}>
+                                   Contact us
+                              </Text>
+                              <UnorderedList lineHeight="2">
+                                   <ListItem>
+                                        27 Division St, New York, NY 10002
+                                   </ListItem>
+                                   <ListItem>+1 800 123 456 78</ListItem>
+                                   <ListItem>bili@mail.com</ListItem>
+                              </UnorderedList>
+                         </Box>
+                         <Box my={10}>
+                              <Text textStyle="title" mb={3}>
+                                   Subscribe
+                              </Text>
+                              <Input
+                                   variant="flushed"
+                                   focusBorderColor="none"
+                                   w="100%"
+                                   backgroundColor="transparent"
+                                   borderBottomColor="mainGrey"
+                                   placeholder="Get news & Updates"
+                                   color="white"
+                                   fontSize="18px"
+                                   fontWeight="bold"
+                                   mb="30px"
+                              />
+                              <Text fontSize="14px" color="titleGrey">
+                                   Our expertise, as well as our passion for web
+                                   design, sets us apart from other agencies.
+                              </Text>
+                              <Stack
+                                   direction="row"
+                                   mt="50px"
+                                   className="profile__social"
+                                   gap={3}
+                              >
+                                   {footerIcon.map(({ ...props }, index) => {
+                                        return (
+                                             <NextLink
+                                                  href="/"
+                                                  passHref
+                                                  key={index}
+                                             >
+                                                  <Link
+                                                       fontSize="20px"
+                                                       _hover={{
+                                                            color: "mainOrange",
+                                                       }}
+                                                  >
+                                                       {props.icons}
+                                                  </Link>
+                                             </NextLink>
+                                        );
+                                   })}
+                              </Stack>
+                         </Box>
+                    </Box>
                </Box>
-          </Box>
+          </>
      );
 };
 
